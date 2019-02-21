24a05cc5f4a326e32d55a119b807aa639c6e00d8
Shreya Sheel <ssheel@ucsd.edu>
2019-02-21 10:52:17 -0800

    almost finished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,12 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+$("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e){
+e.preventDefault();
+ga("send", "event", 'like', 'click');
+	
+}
+
